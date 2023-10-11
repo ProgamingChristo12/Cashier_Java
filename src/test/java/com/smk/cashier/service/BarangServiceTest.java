4d9004e448153b680c3255325b31ae3021dbdb33
ProgramingChristo12 <Christopherateraputra@gmail.com>
2023-10-11 14:33:59 +0700

    Adding and init DAO

--- src/test/java/com/smk/cashier/service/BarangServiceTest.java
+++ src/test/java/com/smk/cashier/service/BarangServiceTest.java
@@ -1,11 +1,13 @@
 package com.smk.cashier.service;
 
+import com.smk.cashier.dao.BarangDao;
 import com.smk.cashier.model.Barang;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,5 +39,26 @@ class BarangServiceTest {
         mouse.setHargaBarang(250000);
         BarangService.getInstance()
                 .addBarang(mouse);
+    }
+
+    @Test
+    @Order(4)
+    void saveBarangtoDatabase(){
+        BarangDao barangDao = new BarangDao();
+        Barang laptop = new Barang();
+        laptop.setKodeBarang("LP001");
+        laptop.setNamaBarang("laptop");
+        laptop.setHargaBarang(5000000);
+        laptop.setDateCreated(new Date());
+        laptop.setLastModifed(new Date());
+       barangDao.save(laptop);
+
+        Barang mouse = new Barang();
+        mouse.setKodeBarang("M0001");
+        mouse.setNamaBarang("Mouse");
+        mouse.setHargaBarang(250000);
+        mouse.setDateCreated(new Date());
+        mouse.setLastModifed(new Date());
+        barangDao.save(mouse);
     }
 }
